e84dda5b1f94aa1f23b97fc91822f5ea76cb2ca9
rasulkarimov <rasul.karimov@gmail.com>
2021-01-21 00:15:23 +0300

    highly avalable webserver ongoing

---
+++ main.tf
@@ -0,0 +1,56 @@
+#---------------------------------------------------
+#
+#---------------------------------------------------
+provider "aws" {
+  region = "eu-north-1"
+}
+
+  data "aws_aviability_zones" "available" {}
+  data "aws_ami" "latest_amazon_linux" {
+  owners      = ["amazon"]
+  most_recent = true
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+}
+
+resource "aws_security_group" "web" {
+  name = "Dynamic SGroup"
+
+  dynamic "ingress" {
+    for_each = ["80", "443"]
+    content {
+      from_port   = ingress.value
+      to_port     = ingress.value
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name  = "Dynamic SGroup"
+    Owner = "Rasul Karimov"
+  }
+}
+
+resource "aws_launch_configuration" "web" {
+  //  name            = "WebServer-Highly-Available-LC"
+  name_prefix     = "WebServer-Highly-Available-LC-"
+  image_id        = data.aws_ami.latest_amazon_linux.id
+  instance_type   = "t3.micro"
+  security_groups = [aws_security_group.web.id]
+  user_data       = file("user_data.sh")
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+
